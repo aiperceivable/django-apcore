bfa46a007ed915be0d5715cae17dd84d0d91a3c1
tercel <tercel.yi@gmail.com>
2026-02-27 17:04:26 +0800

    fix: Remove outdated version check from test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -10,12 +10,6 @@ def test_package_version_exists():
     assert len(django_apcore.__version__) > 0
 
 
-def test_version():
-    import django_apcore
-
-    assert django_apcore.__version__ == "0.1.0"
-
-
 def test_package_importable():
     """django_apcore can be imported without errors."""
     import django_apcore  # noqa: F401
